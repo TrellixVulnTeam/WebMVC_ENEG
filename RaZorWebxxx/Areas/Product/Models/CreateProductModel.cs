970bffbe8ca1641b42caeec2c48fe5cea3a7e76f
manhld62 <38492152+manhld62@users.noreply.github.com>
2022-08-25 20:29:29 +0700

    first commit

---
+++ RaZorWebxxx/Areas/Product/Models/CreateProductModel.cs
@@ -0,0 +1,19 @@
+﻿using RaZorWebxxx.Models.Blog;
+using RaZorWebxxx.Models.Product;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RaZorWebxxx.Areas.Products.Models
+{
+    public class CreateProductModel : ProducModel
+    {
+        [Display(Name="Chuyên Mục")]
+        public int[] CategoryIDs { set; get; }
+
+    }
+   
+    
+}
